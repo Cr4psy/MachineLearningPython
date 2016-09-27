e824de9b86597b314ac9d335c086bcdd26a3dd12
Eduardo Criado Mascaray <ecriado95@gmail.com>
2016-09-27 09:05:27 +0200

    Module name was incorrect.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from cvxopt.solvers import qp
 from cvxopt.base import matrix
-from linKernel import linKernel
+from kernel import LinearK
 
 import numpy, pylab, random, math
 
 
-P=[1,2,3]
+P = [1,2,3]
 
-C=matrix(P)
+C = matrix(P)
 
 classA = [(random.normalvariate(-1.5,1),random.normalvariate(0.5,1),1.0) for i in range(5)] + \
    [(random.normalvariate(1.5,1),random.normalvariate(0.5,1),1.0) for i in range(5)]
@@ -24,4 +24,4 @@ pylab.plot([p[0] for p in classA],[p[1] for p in classA],'bo')
 pylab.plot([p[0] for p in classB],[p[1] for p in classB], 'ro')
 pylab.show()
 
-linKernel(1,2)
+LinearK(1,2)
