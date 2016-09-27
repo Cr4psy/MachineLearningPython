c19bd54c577d92ae4cbf0de0dd0d4598f651c031
Eduardo Criado Mascaray <ecriado95@gmail.com>
2016-09-27 14:52:02 +0200

    Qp now works, we have to get the zeros in alpha.

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import numpy, pylab, random, math
 
 data_size = 20
 
-def non_zero_alpha_values(matrix):
-    return []
-
+def non_zero_alpha_values(alpha):
+    non_zero_elements = []
+    for element in alpha:
+        return 0
+ 
 def create_p_matrix(data):
     P = [[0 for x in range(data_size)] for y in range(data_size)]
     for i in range(data_size):
@@ -19,34 +21,35 @@ def create_p_matrix(data):
     return P   
 
 def create_g_matrix():
-    G = numpy.zeros((data_size, data_size), int)
+    G = numpy.zeros((data_size, data_size))
     numpy.fill_diagonal(G, -1)
     return G
 
 def main():
-    classA = [(random.normalvariate(-1.5,1),random.normalvariate(0.5,1),1.0) for i in range(data_size / 4)] + \
-    [(random.normalvariate(1.5,1),random.normalvariate(0.5,1),1.0) for i in range(data_size / 4)]
+    classA = [(random.normalvariate(-1.5,1),random.normalvariate(0.5,1),1.0) \
+    for i in range(data_size / 4)] + [(random.normalvariate(1.5,1), \
+    random.normalvariate(0.5,1),1.0) for i in range(data_size / 4)]
 
-    classB = [(random.normalvariate(0,0.5),random.normalvariate(-0.5,0.5),-1.0) for i in range(data_size / 2)]
+    classB = [(random.normalvariate(0,0.5),random.normalvariate(-0.5,0.5), \
+    -1.0) for i in range(data_size / 2)]
 
     data = classA + classB
     random.shuffle(data)
     P = create_p_matrix(data)
     G = create_g_matrix()
-    q = numpy.zeros((data_size, 1), int)
+    q = numpy.zeros((data_size, 1))
     q.fill(-1)
-    h = [0] * data_size
+    h = numpy.zeros((data_size, 1))
     r = qp(matrix(P), matrix(q), matrix(G), matrix(h))
     alpha = list(r['x'])
     print alpha
-
+    
 #pylab.hold(True)
 #pylab.plot([p[0] for p in classA],[p[1] for p in classA],'bo')
 #pylab.plot([p[0] for p in classB],[p[1] for p in classB], 'ro')
 #pylab.show()
 
-# Build the q vector, G matrix and h vector
-# SIZE IS TEMPORARY
+
 if __name__ == '__main__':
    main()
 
